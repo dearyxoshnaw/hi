a97951a594b2eb8a57d2e29da0201ab7d0f97f69
dearyxoshnaw <102481779+dearyxoshnaw@users.noreply.github.com>
2022-10-23 10:56:26 -0700

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <form action="hi.php">
+        <h2>Contact Us</h2>
+        
+        <input type="text" class="name2" placeholder="Enter Your Name" required/>
+        <br>
+      
+      
+        <input type="password" class="email2" placeholder="Enter Your Password" required/>
+        <br>
+        <button type="submit">Submit</button>
+        
+
+    </form>
+</body>
+</html>
---
+++ hi.php
@@ -0,0 +1,2 @@
+
+<img src="https://c4.wallpaperflare.com/wallpaper/971/69/127/hand-bones-x-ray-wallpaper-preview.jpg"  width=100%; height=150%; m >;
